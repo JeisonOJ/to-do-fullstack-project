dccfe684dac0f249b1fd82047d1a5811b5ae353a
Jeison <jeisonortiz1516@gmail.com>
2024-09-03 10:19:56 -0500

    add taks mapper

---
+++ Backend/to-do/src/main/java/com/jeison/to_do/infrastructure/mappers/TaskMapper.java
@@ -0,0 +1,26 @@
+package com.jeison.to_do.infrastructure.mappers;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import com.jeison.to_do.api.dto.Request.TaskRequest;
+import com.jeison.to_do.api.dto.Response.TaskResponse;
+import com.jeison.to_do.domain.entities.Task;
+
+@Mapper
+public interface TaskMapper {
+
+    TaskResponse toResponse(Task task);
+
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "createdAt", ignore = true),
+            @Mapping(target = "updatedAt", ignore = true)
+    })
+    Task toEntity(TaskRequest rq);
+
+    List<TaskResponse> toResponse(List<Task> tasks);
+}
