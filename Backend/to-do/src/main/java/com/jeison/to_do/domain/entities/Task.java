2e5d54bbe0766057b6310b4ee3c4644105563862
Jeison <jeisonortiz1516@gmail.com>
2024-09-03 10:17:07 -0500

    create entity task

---
+++ Backend/to-do/src/main/java/com/jeison/to_do/domain/entities/Task.java
@@ -0,0 +1,40 @@
+package com.jeison.to_do.domain.entities;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String title;
+    @Lob
+    private String description;
+    @Column(nullable = false)
+    private boolean completed;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
+}
