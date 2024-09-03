22276442df3b1715b68cc21c8178d6ae38294ab2
Jeison <jeisonortiz1516@gmail.com>
2024-09-03 10:20:05 -0500

    add task service

---
+++ Backend/to-do/src/main/java/com/jeison/to_do/infrastructure/services/TaskService.java
@@ -0,0 +1,56 @@
+package com.jeison.to_do.infrastructure.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jeison.to_do.api.dto.Request.TaskRequest;
+import com.jeison.to_do.api.dto.Response.TaskResponse;
+import com.jeison.to_do.domain.entities.Task;
+import com.jeison.to_do.domain.repositories.TaskRepository;
+import com.jeison.to_do.infrastructure.abstract_services.ITaskService;
+import com.jeison.to_do.infrastructure.mappers.TaskMapper;
+import com.jeison.to_do.utils.IdNotFoundException;
+
+@Service
+public class TaskService implements ITaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private TaskMapper taskMapper;
+
+    @Override
+    public List<TaskResponse> getAll() {
+        return taskMapper.toResponse(taskRepository.findAll());
+    }
+
+    @Override
+    public TaskResponse getById(Long id) {
+        return taskMapper.toResponse(findById(id));
+    }
+
+    @Override
+    public TaskResponse create(TaskRequest rq) {
+        return taskMapper.toResponse(taskRepository.save(taskMapper.toEntity(rq)));
+    }
+
+    @Override
+    public TaskResponse update(Long id, TaskRequest rq) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete(Long id) {
+        findById(id);
+        taskRepository.deleteById(id);
+    }
+
+    private Task findById(Long id) {
+        return taskRepository.findById(id).orElseThrow(() -> new IdNotFoundException("Task"));
+    }
+
+}
