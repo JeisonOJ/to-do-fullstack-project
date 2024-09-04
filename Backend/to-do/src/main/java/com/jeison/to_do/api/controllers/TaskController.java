ffae0d8c01f6c210a6992b7fdeb06dcfa4955c5c
Jeison <jeisonortiz1516@gmail.com>
2024-09-04 14:46:41 -0500

    add crosOrigin to accept requests

--- Backend/to-do/src/main/java/com/jeison/to_do/api/controllers/TaskController.java
+++ Backend/to-do/src/main/java/com/jeison/to_do/api/controllers/TaskController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/tasks")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskController {
 
     @Autowired
