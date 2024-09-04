8fa23bfe5a1daa61fa8aa5ca9137130b5fb0ce82
Jeison <jeisonortiz1516@gmail.com>
2024-09-04 08:31:37 -0500

    add import boxicons, update header bg

--- Frontend/to-do-frontend/src/app/components/Header.tsx
+++ Frontend/to-do-frontend/src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="flex items-center justify-center p-4">
+    <header className="flex items-center justify-center p-4 bg-gradient-to-r from-purple-100 to-pink-100 py-8">
       <h1 className="text-7xl font-bold text-gray-500">To-do App</h1>
     </header>
   );
--- Frontend/to-do-frontend/src/app/layout.tsx
+++ Frontend/to-do-frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import Header from "./components/Header";
 import "./globals.css";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
