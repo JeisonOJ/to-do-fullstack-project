335405765dbebcfe36d3121cce3628ee231e1bd1
Jeison <jeisonortiz1516@gmail.com>
2024-09-04 09:05:20 -0500

    update style to not-found page

--- Frontend/to-do-frontend/src/app/not-found.tsx
+++ Frontend/to-do-frontend/src/app/not-found.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <main className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
-      <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
-      <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+    <main className="h-screen w-full flex flex-col  items-center bg-gradient-to-r from-purple-100 to-pink-100 py-8">
+      <h1 className="text-9xl font-extrabold text-black tracking-widest">404</h1>
+      <div className="bg-purple-600 px-2 text-sm rounded rotate-12 absolute text-white">
         Page Not Found
       </div>
       <button className="mt-5">
-        <div className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
-          <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+        <div className="relative inline-block text-sm font-medium text-white group focus:outline-none focus:ring">
+          <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-purple-600 group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
-          <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+          <span className="relative block px-8 py-3 bg-purple-600 border border-current">
             <Link href="/to-do">Go Home</Link>
           </span>
         </div>
