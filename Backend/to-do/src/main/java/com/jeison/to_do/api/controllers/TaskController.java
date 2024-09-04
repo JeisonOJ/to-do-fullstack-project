e236e7eeb57b9942a936cd32c545f178cabd0e27
Jeison <jeisonortiz1516@gmail.com>
2024-09-04 16:13:18 -0500

    add Requestbody and validated annotations

--- Backend/to-do/src/main/java/com/jeison/to_do/api/controllers/TaskController.java
+++ Backend/to-do/src/main/java/com/jeison/to_do/api/controllers/TaskController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,11 +25,13 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 @RequestMapping("/tasks")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class TaskController {
 
     @Autowired
@@ -43,7 +48,7 @@ public class TaskController {
             @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResp.class))
     })
     @GetMapping("/{id}")
-    public ResponseEntity<TaskResponse> getById(Long id) {
+    public ResponseEntity<TaskResponse> getById(@PathVariable Long id) {
         return ResponseEntity.ok(taskService.getById(id));
     }
 
@@ -52,7 +57,8 @@ public class TaskController {
             @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResp.class))
     })
     @PostMapping
-    public ResponseEntity<TaskResponse> create(TaskRequest rq) {
+    public ResponseEntity<TaskResponse> create(@Validated @RequestBody TaskRequest rq) {
+        log.info("=========================================name {} description {}", rq.getTitle(), rq.getDescription());
         return ResponseEntity.ok(taskService.create(rq));
     }
 
@@ -61,7 +67,7 @@ public class TaskController {
             @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResp.class))
     })
     @PutMapping("/{id}")
-    public ResponseEntity<TaskResponse> update(Long id, TaskRequest rq) {
+    public ResponseEntity<TaskResponse> update(@PathVariable Long id, @Validated @RequestBody TaskRequest rq) {
         return ResponseEntity.ok(taskService.update(id, rq));
     }
 
@@ -71,7 +77,7 @@ public class TaskController {
             @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResp.class))
     })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         taskService.delete(id);
         return ResponseEntity.noContent().build();
     }
