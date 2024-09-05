ced8fd2367e39a4494a9f2af66e6367e020b9a12
Jeison <jeisonortiz1516@gmail.com>
2024-09-05 11:35:56 -0500

    update title color

--- Frontend/to-do-frontend/src/app/components/Header.tsx
+++ Frontend/to-do-frontend/src/app/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="flex items-center justify-center p-4 bg-gradient-to-r from-purple-100 to-pink-100 py-8">
-      <h1 className="text-7xl font-bold text-gray-500">To-Do App</h1>
+      <h1 className="text-7xl font-bold text-purple-950">To-Do App</h1>
     </header>
   );
 }
